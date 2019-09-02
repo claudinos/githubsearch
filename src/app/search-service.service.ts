0951953a717b2d91a1cf4e724130af168ba85a04
Claudine <claudinos1990@gmail.com>
2019-09-02 16:13:19 +0200

    add description

--- src/app/search-service.service.ts
+++ src/app/search-service.service.ts
@@ -1,52 +1,52 @@
-import { Injectable } from '@angular/core';
-import {Search} from './search'
-import { User } from './user';
-import {Repository} from './repository'
-import { from } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import{environment} from '../environments/environment'
+// import { Injectable } from '@angular/core';
+// import {Search} from './search'
+// import { User } from './user';
+// import {Repository} from './repository'
+// import { from } from 'rxjs';
+// import { HttpClient } from '@angular/common/http';
+// import{environment} from '../environments/environment'
 
-@Injectable({
-  providedIn: 'root'
-})
-export class SearchServiceService {
- user:User;
- userName:string;
- answer:string;
- repository:Repository[];
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class SearchServiceService {
+//  user:User;
+//  userName:string;
+//  answer:string;
+//  repository:Repository[];
  
 
-  constructor(private http:HttpClient) {
-    this.user=new User("","","","","",new Date());
-   }
-githubUser(userName){
+//   constructor(private http:HttpClient) {
+//     this.user=new User("","","","","",new Date());
+//    }
+// githubUser(userName){
 
-  interface ApiResponse{
-    login:string;
-    name:string;
-     bio:string;
-     location:string;
-     avatar_url:string;
-     created_at:Date;
-  }
-  let promise = new Promise((resolve,reject)=>{
-    this.http.get<ApiResponse>('https://api.github.com/users/'+ userName + "?access_token=" + environment.apiUrl).toPromise().then(answer=>{
-      this.user.login = answer.login
-      this.user.name = answer.name
-      this.user.bio = answer.bio
-      this.user.location = answer.location
-      this.user.avatar_url = answer.avatar_url
-      this.user. created_at = answer. created_at
+//   interface ApiResponse{
+//     login:string;
+//     name:string;
+//      bio:string;
+//      location:string;
+//      avatar_url:string;
+//      created_at:Date;
+//   }
+//   let promise = new Promise((resolve,reject)=>{
+//     this.http.get<ApiResponse>('https://api.github.com/users/'+ userName + "?access_token=" + environment.apiUrl).toPromise().then(answer=>{
+//       this.user.login = answer.login
+//       this.user.name = answer.name
+//       this.user.bio = answer.bio
+//       this.user.location = answer.location
+//       this.user.avatar_url = answer.avatar_url
+//       this.user. created_at = answer. created_at
    
-      resolve()
-    },
-    error=>{
+//       resolve()
+//     },
+//     error=>{
     
-      this.user.login = "invalid login"
-      reject(error)
-    })
-  })
-  return promise
-  }
+//       this.user.login = "invalid login"
+//       reject(error)
+//     })
+//   })
+//   return promise
+//   }
   
-}
+// }
